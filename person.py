aff4580bff3dc927dd15dc067b6f33eb8bd2fdc9
rotemzaif <rotem8zaif@gmail.com>
2023-01-10 09:14:52 +0200

    add person_type method to person class, modified student and teacher init method, added repr method to student and teacher class

--- person.py
+++ person.py
@@ -1,29 +1,33 @@
 class Person:
-    def __init__(self, full_name, identity_num):
+    def __init__(self, identity_number, full_name):
         self.name = full_name
-        self.identity_num = identity_num
+        self.identity_num = identity_number
         self.courses = []
+
+    def person_type(self):
+        return type(self).__name__
 
 
 class Student(Person):
     def __init__(self, *args, faculty, start_date, address):
-        super().__init__(*args)
         self.faculty = faculty
         self.start_date = start_date
         self.address = address
+        super().__init__(*args)
 
     def __repr__(self):
-        return f'Student({self.name}, {self.identity_num}, {self.courses}, {self.faculty}, {self.address}, {self.start_date})'
+        return f'Student({self.identity_num}, {self.name}, {self.courses}, {self.faculty}, {self.address}, {self.start_date})'
 
 
 class Teacher(Person):
-    def __init__(self, *args, faculties, start_date):
+    def __init__(self, *args, faculty, start_date):
         super().__init__(*args)
-        self.faculties = faculties
+        self.faculties = []
+        self.faculties.append(faculty)
         self.start_date = start_date
 
     def __repr__(self):
-        return f'Teacher({self.name}, {self.identity_num}, {self.faculties}, {self.start_date})'
+        return f'Teacher({self.identity_num}, {self.name}, {self.faculties}, {self.start_date})'
 
 
 
