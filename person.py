091c494a0211a4c3a78267d573e82a61c2ac986d
rotemzaif <rotem8zaif@gmail.com>
2023-01-11 17:32:27 +0200

    modified Person class person_type method, added add_course method; added get_total_courses_points method to student class; modified Teacher class init, added add_course method with override from parent class

--- person.py
+++ person.py
@@ -5,7 +5,11 @@ class Person:
         self.courses = []
 
     def person_type(self):
-        return type(self).__name__
+        # return type(self).__name__
+        return self.__class__.__name__
+
+    def add_course(self, course_id):
+        self.courses.append(course_id)
 
 
 class Student(Person):
@@ -15,6 +19,12 @@ class Student(Person):
         self.address = address
         super().__init__(*args)
 
+    def get_total_courses_points(self):
+        total = 0
+        for course in self.courses:
+            total += course.points
+        return total
+
     def __repr__(self):
         return f'Student(ID: {self.identity_num}, full name: {self.name}, courses: {self.courses}, faculty: {self.faculty}, ' \
                f'address: {self.address}, start_date: {self.start_date})'
@@ -23,9 +33,13 @@ class Student(Person):
 class Teacher(Person):
     def __init__(self, *args, faculty, start_date):
         super().__init__(*args)
-        self.faculties = []
-        self.faculties.append(faculty)
+        self.faculties = [faculty]
         self.start_date = start_date
+
+    def add_course(self, course_id, course_faculty=None):
+        self.courses.append(course_id)
+        if course_faculty is not None and course_faculty not in self.faculties:
+            self.faculties.append(course_faculty)
 
     def __repr__(self):
         return f'Teacher(ID: {self.identity_num}, full name: {self.name}, faculties: {self.faculties}, start date: {self.start_date})'
